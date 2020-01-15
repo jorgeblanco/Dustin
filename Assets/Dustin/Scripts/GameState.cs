8d3d5b2f452e9864951c5985f7f0ba7bc5c7e1bf
Jorge Blanco <jbsfreelance@gmail.com>
2020-01-15 00:55:57 -0800

    Added prefabs from the vikings polygon pack and fixed an issue with the distance grabber from the Oculus framework.
    Reference: https://forum.unity.com/threads/vr-oculus-sdk-touch-controller-snap-position-snap-offset-grab-a-gun.512853/#post-3594758
    
    // Replace
    
    m_grabbedObjectPosOff = m_gripTransform.localPosition;
    if(m_grabbedObj.snapOffset)
    {
        Vector3 snapOffset = m_grabbedObj.snapOffset.position;
        if (m_controller == OVRInput.Controller.LTouch) snapOffset.x = -snapOffset.x;
        m_grabbedObjectPosOff += snapOffset;
    }
    
    // AND
    
    m_grabbedObjectRotOff = m_gripTransform.localRotation;
    if(m_grabbedObj.snapOffset)
    {
        m_grabbedObjectRotOff = m_grabbedObj.snapOffset.rotation * m_grabbedObjectRotOff;
    }
    
    // WITH
    
    if (m_grabbedObj.snapOffset)
    {
        Vector3 snapOffset = -m_grabbedObj.snapOffset.localPosition;
        Vector3 snapOffsetScale = m_grabbedObj.snapOffset.lossyScale;
        snapOffset = new Vector3(snapOffset.x * snapOffsetScale.x, snapOffset.y * snapOffsetScale.y, snapOffset.z * snapOffsetScale.z);
        if (m_controller == OVRInput.Controller.LTouch)
        {
            snapOffset.x = -snapOffset.x;
        }
        m_grabbedObjectPosOff = snapOffset;
    }
    else
    {
        m_grabbedObjectPosOff = Vector3.zero;
    }
    
    // AND
    
    if (m_grabbedObj.snapOffset)
    {
        m_grabbedObjectRotOff = Quaternion.Inverse(m_grabbedObj.snapOffset.localRotation);
    }
    else
    {
        m_grabbedObjectRotOff = Quaternion.identity;
    }

---
+++ Assets/Dustin/Scripts/GameState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameState : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ReloadScene();
+        }
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
